bd117c554c3d1213c48b01aa5b5e4df4fd3ca989
Alex Buniakin <lexx@MacBook-Pro-Alex.local>
2022-12-23 10:35:28 +0200

    pre-deployment changes

--- colibri/settings.py
+++ colibri/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import mimetypes
+import os
 
 
 mimetypes.add_type('application/javascript', '.js', True)
@@ -24,12 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*5yw-15js3my5xv2_@eakhc)7us7+@vv7y!16)t!p^rl__mh@^'
+# SECRET_KEY = 'django-insecure-*5yw-15js3my5xv2_@eakhc)7us7+@vv7y!16)t!p^rl__mh@^'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-*5yw-15js3my5xv2_@eakhc)7us7+@vv7y!16)t!p^rl__mh@^')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,9 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+"""
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+"""
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
